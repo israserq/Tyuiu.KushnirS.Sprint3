5ad3e4a0ad8c63ab841e361657b9ebaba4b7be3d
User <User@DESKTOP-8Q26HPT>
2024-10-31 23:17:03 +0500

    Task2

---
+++ Tyuiu.KushnirS.Sprint3.Task2.V24.Test/DataServicetest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.KushnirS.Sprint3.Task2.V24.Lib;
+namespace Tyuiu.KushnirS.Sprint3.Task2.V24.Test
+{
+    [TestClass]
+    public class DataServicetest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 8;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 55217.446;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KushnirS.Sprint3.Task2.V24/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.KushnirS.Sprint3.Task2.V24.Lib;
+namespace Tyuiu.KushnirS.Sprint3.Task2.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 8;
+            Console.WriteLine("Перемнная x = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = Convert.ToDouble(ds.GetMultiplySeries(value, startValue, stopValue));
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
